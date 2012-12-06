bad2970650c91c44d818bc76a538e534b687e2e3
larryeitel <larry@eitel.com>
2012-12-06 09:38:45 -0600

    tweekage

--- flask_seed/models/mydocument.py
+++ flask_seed/models/mydocument.py
@@ -1,51 +1,34 @@
 from app import app
 from mongoengine.base import ValidationError
 
+def validate(doc):
+    errors = {}
+
+    # Get a list of tuples of field names and their current values
+    fields = [(field, getattr(doc, name))
+              for name, field in doc._fields.items()]
+
+    # Ensure that each field is matched to a valid value
+    errors = {}
+    for field, value in fields:
+        if value is not None:
+            try:
+                field._validate(value)
+            except ValidationError, error:
+                errors[field.name] = error.errors or error
+            except (ValueError, AttributeError, AssertionError), error:
+                errors[field.name] = error
+        elif field.required:
+            errors[field.name] = ValidationError('Field is required',
+                                                 field_name=field.name)
+    return errors
+
+# inspired by http://stackoverflow.com/questions/6102103/using-mongoengine-document-class-methods-for-custom-validation-and-pre-save-hook
 class MyDocument(app.db.Document):
-    # inspired by http://stackoverflow.com/questions/6102103/using-mongoengine-document-class-methods-for-custom-validation-and-pre-save-hook
     def validate(self):
-        errors = {}
-
-        # Get a list of tuples of field names and their current values
-        fields = [(field, getattr(self, name))
-                  for name, field in self._fields.items()]
-
-        # Ensure that each field is matched to a valid value
-        errors = {}
-        for field, value in fields:
-            if value is not None:
-                try:
-                    field._validate(value)
-                except ValidationError, error:
-                    errors[field.name] = error.errors or error
-                except (ValueError, AttributeError, AssertionError), error:
-                    errors[field.name] = error
-            elif field.required:
-                errors[field.name] = ValidationError('Field is required',
-                                                     field_name=field.name)
-        return errors
-
+        return validate(self)
 
 class MyEmbeddedDocument(app.db.EmbeddedDocument):
     def validate(self):
-        errors = {}
-
-        # Get a list of tuples of field names and their current values
-        fields = [(field, getattr(self, name))
-                  for name, field in self._fields.items()]
-
-        # Ensure that each field is matched to a valid value
-        errors = {}
-        for field, value in fields:
-            if value is not None:
-                try:
-                    field._validate(value)
-                except ValidationError, error:
-                    errors[field.name] = error.errors or error
-                except (ValueError, AttributeError, AssertionError), error:
-                    errors[field.name] = error
-            elif field.required:
-                errors[field.name] = ValidationError('Field is required',
-                                                     field_name=field.name)
-        return errors
+        return validate(self)
 
